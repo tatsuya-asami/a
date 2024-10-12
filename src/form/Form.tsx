a6f04a9526d2b1737cdd0c45cc6927fc2bca8336
tatsuya-asami <asami.tatsuya@commmune.jp>
2024-10-13 00:45:47 +0900

    Refactor Form component and add SubValue component

--- src/form/Form.tsx
+++ src/form/Form.tsx
@@ -1,44 +1,58 @@
-import { useFieldArray, useForm } from "react-hook-form";
+import { FormProvider, useFieldArray, useForm } from "react-hook-form";
+import { SubValue } from "./SubValue";
+import { useState } from "react";
 
 export const Form = () => {
-  const { register, control, handleSubmit } = useForm<FormValues>({
+  const [openSubValueIndex, setOpenSubValueIndex] = useState<number | null>(
+    null
+  );
+  const methods = useForm<FormValues>({
     defaultValues,
   });
+  const { register, control, handleSubmit } = methods;
   const { fields, append, remove } = useFieldArray({
     control,
     name: "myList",
   });
 
   return (
-    <form
-      onSubmit={handleSubmit((data) => {
-        console.log(data);
-      })}
-    >
-      {fields.map((field, index) => {
-        return (
-          <div key={field.id}>
-            <input {...register(`myList.${index}.main`)} />
-            <button type="button" onClick={() => remove(index)}>
-              remove
-            </button>
-          </div>
-        );
-      })}
-      <div>
-        <button type="button" onClick={() => append(defaultMyList)}>
-          append
-        </button>
-      </div>
-      <div>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
+    <FormProvider {...methods}>
+      <form
+        onSubmit={handleSubmit((data) => {
+          console.log(data);
+        })}
+      >
+        {fields.map((field, index) => {
+          return (
+            <div key={field.id}>
+              <input {...register(`myList.${index}.main`)} />
+              <button type="button" onClick={() => remove(index)}>
+                remove
+              </button>
+              <button onClick={() => setOpenSubValueIndex(index)}>
+                open sub input
+              </button>
+              {openSubValueIndex === index && <SubValue index={index} />}
+            </div>
+          );
+        })}
+        <div>
+          <button type="button" onClick={() => append(defaultMyList)}>
+            append
+          </button>
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </FormProvider>
   );
 };
 
-const defaultMyList = { main: "", sub: "" };
-const defaultValues = {
+const defaultMyList = { main: "" };
+const defaultValues: FormValues = {
   myList: [defaultMyList],
 };
-type FormValues = typeof defaultValues;
+export type FormValues = {
+  myList: { main: string; sub?: string }[];
+};
